bed94698efc766cd595ccdc5e816113a5d0e5f2f
Siobhantt <luisacardozold@hotmail.com>
2023-11-16 10:48:45 +0100

    Agregar archivos de proyecto.

---
+++ U8/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using U8.Models;
+
+namespace U8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //El controlador de la vista index
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //El controlador de la vista saludo, que recibe el nombre
+
+        public IActionResult Saludo(String nombre)
+        {
+            //la viewbag que mostrará el nombre en la vista saludo
+            ViewBag.nombre = nombre;
+            return View("Saludo");
+        }
+
+       
+
+    }
+}
